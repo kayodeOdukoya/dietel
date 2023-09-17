7391c34d95f238892d36565d4415ae75b25beea2
Muyiwa Odukoya <kayusyiwex@gmail.com>
2023-09-17 22:03:21 +0100

    commit message

--- playGround.py
+++ playGround.py
@@ -32,20 +32,20 @@
 # for character in "programming":
 #     print(character, end="   ")
                                 #average of class result.
-# total = 0
-# counter = 0
-#
-# grade = int(input("Enter a grade, -2 to stop and run: "))
-# while grade != -2:
-#     total += grade
-#     counter += 1
-#     grade = int(input("Enter a grade, -2 to stop and run: "))
-#
-# if counter != 0:
-#     average = total / counter
-#     print(f"class average is {average}")
-# else:
-#     print("no grade was entered")
+total = 0
+counter = 0
+
+grade = int(input("Enter a grade, -2 to stop and run: "))
+while grade != -2:
+    total += grade
+    counter += 1
+    grade = int(input("Enter a grade, -2 to stop and run: "))
+
+if counter != 0:
+    average = total / counter
+    print(f"class average is {average}")
+else:
+    print("no grade was entered")
 
 
 
