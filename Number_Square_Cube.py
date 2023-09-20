8b11044a53c8766c15e0b7a84c2d645d31deaca6
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-09-20 07:07:27 +0100

    Update Number_Square_Cube.py

--- Number_Square_Cube.py
+++ Number_Square_Cube.py
@@ -13,3 +13,6 @@ while number <= 11:
     cube = number * number * number
     print(f"{number}\t\t{square}\t\t{cube}")
     number += 1
+
+# pls rename the file like a python file
+# pls where are the chapter 2 and 3 exercise.??
