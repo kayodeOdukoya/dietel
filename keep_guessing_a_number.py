06b0f82a4c59dd1827b8973c29ba43365a6d883e
Muyiwa Odukoya <kayusyiwex@gmail.com>
2023-09-21 10:18:40 +0100

    commit message

--- keep_guessing_a_number.py
+++
@@ -1,9 +0,0 @@
-import random
-
-random = random.randint(1, 10)
-guess_number = int(input("Enter a number: "))
-
-while guess_number != random:
-    guess_number = eval(input("keep guess a number: "))
-    if guess_number == random:
-     print("correct, you won")
--- multiplicationTable.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 13):
-    for j in range(2, 21):
-        print(f"{i:>2} * {j:>2} = {i * j:>2}", end="\t\t")
-    print()
