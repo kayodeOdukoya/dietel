a6115b0900641fe7d98a34df967fede07bd540d1
Muyiwa Odukoya <kayusyiwex@gmail.com>
2023-10-09 04:10:01 +0100

    commit message today 9/10/2023

---
+++ tuple/reverse_tuple.py
@@ -0,0 +1,9 @@
+
+def reverse_tuple(self, tuple_to_reverse):
+    reversed_list = []
+    for item in tuple_to_reverse:
+        reversed_list.insert(0, item)
+        return tuple(reversed_list)
+
+    def create_single_element_tuple(self, value):
+        return (value,)
